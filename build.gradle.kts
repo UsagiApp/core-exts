620e9789c05172252b7cc0d740383189babf6c70
dragonx943 <premieregirl26@gmail.com>
2026-03-24 17:20:08 +0700

    Initial commit: Initialize and migrate cores to this project

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
+    `maven-publish`
+}
+
+group = "org.koitharu.kotatsu"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("androidx.collection:collection-ktx:1.4.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp-dnsoverhttps:4.12.0")
+    implementation("com.squareup.okio:okio:3.9.1")
+    implementation("org.jsoup:jsoup:1.18.3")
+    implementation("org.json:json:20240303")
+}
+
+kotlin {
+    jvmToolchain(17)
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            "-opt-in=kotlin.RequiresOptIn",
+            "-opt-in=kotlin.contracts.ExperimentalContracts",
+            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+            "-opt-in=org.koitharu.kotatsu.parsers.InternalParsersApi"
+        )
+    }
+}
