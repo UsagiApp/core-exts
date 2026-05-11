87ee0d5591463b7bb9d6c6405b44341fd14de6ff
dragonx943 <premieregirl26@gmail.com>
2026-05-11 14:23:35 +0700

    Fix backward compatibility

--- src/main/kotlin/org/koitharu/kotatsu/parsers/model/MangaSource.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/model/MangaSource.kt
@@ -3,8 +3,8 @@ package org.koitharu.kotatsu.parsers.model
 public interface MangaSource {
 
 	public val name: String
-	public val title: String
-	public val locale: String
-	public val contentType: ContentType
-	public val isBroken: Boolean
+	public val title: String get() = name
+	public val locale: String get() = ""
+	public val contentType: ContentType get() = ContentType.OTHER
+	public val isBroken: Boolean get() = false
 }
